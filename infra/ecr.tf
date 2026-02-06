93e688c79dc5a4360d9fbba87bc7a57958060d83
Vighnesh Kadam <vighnesh.kadam.se@gmail.com>
2026-02-06 08:31:44 +0000

    first commit

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,7 +1,9 @@
 resource "aws_ecr_repository" "market_data" {
   name = "market-data-ingestion"
+  force_delete = true
 }
 
 resource "aws_ecr_repository" "order_book" {
   name = "order-book-state"
+  force_delete = true
 }
